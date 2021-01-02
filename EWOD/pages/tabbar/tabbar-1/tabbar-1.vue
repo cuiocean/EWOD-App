98c609ea18d877b9284210a8604151d71fc08346
haiyangcui <cuiocean@gmail.com>
2021-01-02 20:46:36 +0100

    首页加入"蓝牙设备"按钮

--- EWOD/pages/tabbar/tabbar-1/tabbar-1.vue
+++ EWOD/pages/tabbar/tabbar-1/tabbar-1.vue
@@ -1,6 +1,14 @@
 <template>
 	<view class="content">
-		页面 - 1
+		<view class="info">
+			点击连接蓝牙设备
+		</view>
+		<view class="button">
+			<u-button type="primary" :ripple="true">蓝牙设备</u-button>
+		</view>
+		<view class="info">
+			文字说明设备名称,如何链接,等等
+		</view>
 	</view>
 </template>
 
